2107629080f7e89a22cf4dd9ff17f525758736e5
Corned <oliver@kaarnalehto.fi>
2019-09-04 20:11:24 +0300

    modal + button css

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,14 +1,29 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import posed from "react-pose"
 
 import "./index.css"
+
+const Button = posed.button({
+  hoverable: true,
+
+  init: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.2,
+  },
+})
+
 
 const Home = () => {
   return (
     <div className="home">
       <h1 className="header--giant">Welcome to draw.owo!</h1>
-      <h2>Join the public drawing room</h2>
-      <Link to="/public">public</Link>
+      <div className="">
+        <button className="">join the public room</button>
+        <button className="">create a private room</button>
+      </div>
     </div>
   )
 }
