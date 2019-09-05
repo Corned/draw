4fc5215ea4cf4148441465768c6b007315307294
Corned <oliver@kaarnalehto.fi>
2019-09-05 18:13:34 +0300

    fix for joining private rooms

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io = socketio(server)
 app.use(express.static(path.join(__dirname, "build")))
 app.use("/api", require("./api/RoomApi")(io))
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(
     path.join(__dirname, "build", "index.html")
   )
